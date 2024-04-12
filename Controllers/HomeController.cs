54e4a40b7d763aea62dcb0b0d7dda351e878242f
alex <alexdim1810@gmail.com>
2024-04-13 01:26:18 +0300

    Submit monthly budget and show red message if over it

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using LateNight.Models;
-namespace LaneNight.Controllers
+using System.Linq; // Necessary for LINQ operations
+
+namespace LateNight.Controllers
 {
     public class HomeController : Controller
     {
         static List<Expense> expenses = new List<Expense>();
+        static decimal monthlyBudget = 0; // Static field to store the monthly budget
+        static bool isBudgetSet = false; // Flag to check if budget is already set
 
         public ActionResult Index()
         {
+            ViewBag.TotalExpenses = expenses.Sum(e => e.Amount);
+            ViewBag.Budget = monthlyBudget;
+            ViewBag.OverBudget = ViewBag.TotalExpenses > ViewBag.Budget ? ViewBag.TotalExpenses - ViewBag.Budget : 0;
+            ViewBag.IsBudgetSet = isBudgetSet; // Ensure this is always set
+
             return View(expenses);
         }
 
@@ -16,6 +25,17 @@ namespace LaneNight.Controllers
         public ActionResult AddExpense(Expense expense)
         {
             expenses.Add(expense);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult SetBudget(decimal budget)
+        {
+            if (!isBudgetSet) // Only set the budget if it hasn't been set before
+            {
+                monthlyBudget = budget;
+                isBudgetSet = true;
+            }
             return RedirectToAction("Index");
         }
     }
--- Models/Expense.cs
+++ Models/Expense.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LateNight.Models
 {
     public class Expense
     {
+        [Required(ErrorMessage = "The name of the expense is required.")]
+        [StringLength(100, ErrorMessage = "The name must be less than 100 characters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "The amount of the expense is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "The amount must be greater than zero.")]
         public decimal Amount { get; set; }
     }
 }
